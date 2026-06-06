605424930c28eaf03adcb9b355819e396724e935
Ken Yamashita <feruos@gmail.com>
2026-06-07 06:29:34 +0900

    json_io refactored

--- data_io/json_io.py
+++ data_io/json_io.py
@@ -19,9 +19,9 @@ class JsonEntity(ABC):
     def asEntity(self)->list[str]:
         pass
 
-    def __json_literal(value) -> str:
+    def json_literal(self, value: str|int|float|bool) -> str:
         return json.dumps(value, ensure_ascii=False)
-            
+
 class Array(JsonEntity):
     def __init__(self,
                 key:str=None,
@@ -41,7 +41,7 @@ class Array(JsonEntity):
         list_json_str:list[str] = []
         list_json_str.append('{')
         if self.description is not None:
-            list_json_str.append(f' "description":"{self.description}",')
+            list_json_str.append(f' "description":{self.json_literal(self.description)},')
         list_json_str.append(' "type":"array",')
 
         if isinstance(self.content, JsonEntity):
@@ -59,13 +59,17 @@ class Array(JsonEntity):
             list_json_str[-1]=list_json_str[-1].removesuffix(',')
             list_json_str.append('  ]')
             list_json_str.append(' }')
+        else:
+            raise TypeError(f'{self.__class__.__name__}: invalid "content" type')
+
         list_json_str.append('}')
         return list_json_str
     
     def asEntity(self)->list[str]:
         list_json_str:list[str] = self.asType()
-        list_json_str[0] =f'"{self.key}":'+list_json_str[0]
+        list_json_str[0] =f'{self.json_literal(self.key)}:'+list_json_str[0]
         return list_json_str
+
             
 class Objason(JsonEntity):
     def __init__(self,
@@ -84,7 +88,7 @@ class Objason(JsonEntity):
         list_json_str: list[str] = []
         list_json_str.append('{')
         if self.description is not None:
-            list_json_str.append(f' "description":"{self.description}",')
+            list_json_str.append(f' "description":{self.json_literal(self.description)},')
         list_json_str.append(' "type":"object",')
         list_json_str.append(' "properties":{')
         required:str=''
@@ -93,7 +97,7 @@ class Objason(JsonEntity):
                 list_json_str.append('  '+line)
             list_json_str[-1] += ','
             if single_prop.required:
-                required += f'"{single_prop.key}",'
+                required += f'{self.json_literal(single_prop.key)},'
         list_json_str[-1]=list_json_str[-1].removesuffix(',')
         list_json_str.append(' }')
 
@@ -108,58 +112,48 @@ class Objason(JsonEntity):
 
     def asEntity(self)->list[str]:
         list_json_str:list[str] = self.asType()
-        list_json_str[0] =f'"{self.key}":'+list_json_str[0]
+        list_json_str[0] =f'{self.json_literal(self.key)}:'+list_json_str[0]
         return list_json_str
+
         
 class Primitive(JsonEntity):
     def __init__(self,
                  prim_type:Literal['string','integer', 'number','boolean'] = None,
                  key:str = None,
                  description:str = None,
-                 enum:list[str]|list[int]|list[float] = None,
+                 enum:list[str]|list[int]|list[float]|list[bool] = None,
                  const:str|int|float|bool = None,
                  required:bool = True):
         super().__init__(key=key, description=description, required=required)
         self.prim_type:str = prim_type
-        self.enum:list[str]|list[int]|list[float] = enum
+        self.enum:list[str]|list[int]|list[float]|list[bool] = enum
         self.const:str|int|float|bool = const
         if key is None:
             raise ValueError(f'{self.__class__.__name__}: "key" not given.')  
         if enum is not None and const is not None:
             raise ValueError(f'{self.__class__.__name__}: "enum" and "const" are given at the same time.')
-        if True in enum or False in enum:
-            raise ValueError(f'{self.__class__.__name__}: Python-style boolean True/False cannot be used as a value for JSON.')
         if prim_type is None:
             raise ValueError(f'{self.__class__.__name__}: Type not selected.')
-        if const is True:
-            const = 'true'
-        elif const is False:
-            const = 'false'
+        # if const is True:
+        #     const = 'true'
+        # elif const is False:
+        #     const = 'false'
 
     
     def asType(self)->list[str]:
         list_json_str: list[str] = []
         list_json_str.append('{')
         if self.description is not None:
-            list_json_str.append(f' "description":"{self.description}",')
+            list_json_str.append(f' "description":{self.json_literal(self.description)},')
         list_json_str.append(f' "type":"{self.prim_type}",')
+
         if self.enum is not None:
-            str_enum:str = ' "enum":['
-            if self.prim_type != 'integer' and self.prim_type != 'number' and self.prim_type !='boolean':
-                for item in self.enum:
-                    str_enum += f'"{item}",'
-            else:
-                for item in self.enum:
-                    str_enum += f'{item},'
-            str_enum = str_enum.removesuffix(',')
-            str_enum += '],'
-            list_json_str.append(str_enum)
+            enum_values = ','.join(self.json_literal(item) for item in self.enum)
+            list_json_str.append(f' "enum":[{enum_values}],')
+
         if self.const is not None:
-            delim:str = ''
-            if self.prim_type != 'integer' and self.prim_type != 'number' and self.prim_type !='boolean':
-                delim = '"'
-            list_json_str.append(f' "const":{delim}{self.const}{delim},')
-        
+            list_json_str.append(f' "const":{self.json_literal(self.const)},')
+
         list_json_str[-1]=list_json_str[-1].removesuffix(',')
         list_json_str.append('}')
         return list_json_str
@@ -167,5 +161,7 @@ class Primitive(JsonEntity):
 
     def asEntity(self)->list[str]:
         list_json_str:list[str] = self.asType()
-        list_json_str[0] =f'"{self.key}":'+list_json_str[0]
+        list_json_str[0] =f'{self.json_literal(self.key)}:'+list_json_str[0]
         return list_json_str
+    
+            
